b0048835d97c9fbb8098d670691b2d3a1bd81632
Jeremy Kolassa <jdkolassa@gmail.com>
2022-01-16 11:47:20 -0500

    Custom Post Type 'Star'
    Initial build of a custom post type to hold our star data

---
+++ plugins/astrographer/includes/custom_star_type.php
@@ -0,0 +1,70 @@
+<?php
+
+// Create a custom post type for our stars
+// ! Use "star_post_type" to avoid confusion with star spectral types
+
+function astrog_create_star_post_type() {
+    register_post_type('astrog_star',
+        [
+            'labels' => [
+                'name' => __('Stars', 'textdomain'),
+                'singular_name' => __('Star', 'textdomain'),
+            ],
+            'public' => true,
+            'has_archive' => false,
+            'rewrite' => ['slug' => 'stars'],
+        ]
+        );
+}
+
+add_action('init', 'astrog_create_star_post_type');
+
+// Let's create the metaboxes with cmb2
+
+function cmb2_star_metaboxes(){
+
+    // Init the metabox itself
+
+    $cmb = new_cmb2_box([
+        'id' => 'starbox',
+        'title' => __('Astrographer Metabox', 'cmb2'),
+        'object_types' => ['astrog_star'],
+        'context' => 'normal',
+        'priority' => 'high',
+        'show_name' => 'true' // show field names
+
+    ]);
+
+    // Let's quickly create some text fields
+
+    $textfields = [
+        'name',
+        'spectral_type',
+        'distance',
+    ];
+
+    foreach($textfields as $field) {
+        $cmb->add_field([
+            'name' => ucfirst(str_replace("_", " ", $field)),
+            'id' => 'astrog_' . $field,
+            'type' => 'text',
+        ]);
+    }
+
+    // CMB2 does not have a boolean field type, and I don't see an extension that adds it, so I'll use a simple radio field instead
+    // ! Planets are only in the Solar System. All other similar objects are specifically exoplanets.
+    // ! Astronomers get annoyed if you conflate the two.
+    // TODO: Stretch goal: create planet post type and get more details
+
+    $cmb->add_field([
+        'name' => 'Exoplanets',
+        'desc' => 'Does this star have exoplanets?',
+        'id' => 'astrog_exoplanets',
+        'type' => 'radio_inline',
+        'options' => [
+            'no' => __("None detected", "cmb2"), // We say none detected because we don't know if there actually are no exoplanets
+            'yes' => __("Yes", "cmb2"),
+        ],
+        'default' => 'no',
+    ]);
+}
