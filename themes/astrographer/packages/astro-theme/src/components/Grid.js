4fcd474e8628559875d70fc70cf5d2153df1bb35
Jeremy Kolassa <jdkolassa@gmail.com>
2022-02-22 12:31:03 -0500

    Figuring out Grid component

--- themes/astrographer/packages/astro-theme/src/components/Grid.js
+++ themes/astrographer/packages/astro-theme/src/components/Grid.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { connect } from "frontity";
 
-const Grid = ({ state }) => {
-  const data = state.source.get("/astrog_star");
-  return (
-  <main class="container bg-black">
-      <div class="grid grid-flow-col auto-cols-max">
+// TODO: Implement lazy loading
 
+const Grid = ({ state }) => {
+  
+  const stars = state.source.get('/');
+  console.log(stars);
+  
+  return (
+  <div className="container bg-black">
+      <div className="grid grid-flow-col auto-cols-max">
+        <p>DUMMY TEXT FOR NOW</p>
+        <div>
+           {/* {stars.items.map((item) => {
+             const star = state.source[item.type][item.id];
+            return (
+              <Star name={star.title.rendered} spectral={star.spectral_type} distance={star.distance}>
+
+              </Star>
+            )
+          })}  */}
+        </div>
       </div>
-  </main>
+  </div>
   );
 }
 
