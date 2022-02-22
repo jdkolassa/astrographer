282a45a7651a7176c517b86062a7c3f6ada1768e
Jeremy Kolassa <jdkolassa@gmail.com>
2022-02-22 12:30:43 -0500

    Scaffolding home page
    just getting the basics worked out

--- themes/astrographer/packages/astro-theme/src/components/index.js
+++ themes/astrographer/packages/astro-theme/src/components/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { connect } from "frontity";
+import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
+import Grid from "./Grid";
 
 // TODO: Make Frontity pull for "astrog_star" and not "posts" or "pages" (except the about page)
 // TODO: Implement a Switch component to change between the default Grid and the About page
 // TODO: When Grid is functional, scaffold it out and see what it looks like
 
 const Root = ({ state }) => {
+  const data = state.source.get(state.router.link); // at home, this is state.source.get('/')
+
   return (
     <>
+        <header className="bg-black p-10 ">
+          <h1 className="mx-auto flex justify-center items-center h-full">ASTROGRAPHER</h1>
+          <nav className="bg-purple-800 text-white flex place-items-center">
+            <div>
+              <Link link="/stars">Home</Link>
+              <Link link="/about">About</Link>
+            </div>
+          </nav>
+        </header>
         <p>Current page is: {state.router.link}</p>
-        <p></p>
+          <main className="bg-black flex place-items-center text-white">
+            <Switch>
+              <Grid when={data.isHome} />
+              <div when={data.isPage} />
+            </Switch>
+          </main>
+          
+        
     </>
   
   )
