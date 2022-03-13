08bede7dfdd7caa0ea7c81e4f7dfb92a8629816e
Jeremy Kolassa <jdkolassa@gmail.com>
2022-03-13 16:50:20 -0400

    Removed frontity config
    Since I'm no longer stellar classification parser, I no longer need this config.

--- themes/astrographer/packages/astro-theme/frontity.config.js
+++
@@ -1,9 +0,0 @@
-const webpack_1 = require("webpack");
-
-export const webpack = ({ config, target }) => {
-  if (target !== "server") {
-    config.devtool = "eval-cheap-source-map";
-    config.plugins.push(new webpack_1.IgnorePlugin(/fs/, /stellar-classification-parser/));
-  }
-  
-};
